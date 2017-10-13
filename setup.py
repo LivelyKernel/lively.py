04c0b8804082bf75ceab4e2197ca43e2041c14bd
Robert Krahn <robert.krahn@gmail.com>
2017-10-13 08:57:14 -0700

    preparing to publish

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
-from distutils.core import setup
-setup(
-  name = 'lively-toolbox',
-  packages = ['lively-toolbox'],
-  version = '0.1.0',
-  description = 'Lively comes to Python',
-  author = 'Robert Krahn',
-  author_email = 'robert.krahn@gmail.com',
-  url = 'https://github.com/rksm/lively-toolbox-py',
-  download_url = 'https://github.com/rksm/lively-toolbox-py/archive/0.1.0.tar.gz',
-  keywords = ['lively', 'live programming', 'ide', 'programming tools'],
-  classifiers = [],
-)
-
-
-
-
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -43,8 +26,8 @@ setup(
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: ',
-        'Topic :: Text Editors :: Integrated Development Environments (IDE)'
+        'Topic :: Software Development',
+        'Topic :: Text Editors :: Integrated Development Environments (IDE)',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3 :: Only',
     ],
