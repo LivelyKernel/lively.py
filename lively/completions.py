93a20a98d922f1e003d50d89f94bcfd3fd45ac2d
Robert Krahn <robert.krahn@gmail.com>
2018-01-02 17:35:13 -0800

    fix dynamic completions via object inspection

--- lively/completions.py
+++ lively/completions.py
@@ -1,24 +1,25 @@
 import inspect
 import jedi
-from .eval import run_eval
+from lively.eval import run_eval
 
 async def get_completions(source, row, column, file):
     compl_data = []
-    
+
     # try jedi
     script = jedi.Script(source, row, column, file)
     completions = script.completions()
-
     if (len(completions) > 0):
-      compl_attrs = ["module_name", "module_path", "is_keyword", "type", "name", "full_name"]
-      for compl in completions:
-        data = {name: getattr(compl, name) for name in compl_attrs}
-        data['priority'] = 1000
-        if compl.type == "function":
-          signature = "({})".format(",".join([p.name for p in compl.params]))
-          data['name'] += signature
-        compl_data.append(data)
-      return compl_data
+        compl_attrs = ["module_name", "module_path", "is_keyword", "type", "name", "full_name"]
+        counter = 0
+        for compl in completions:
+            data = {name: getattr(compl, name) for name in compl_attrs}
+            data['priority'] = 1000 - counter
+            counter = counter + 1
+            if compl.type == "function":
+                signature = "({})".format(",".join([p.name for p in compl.params]))
+                data['name'] += signature
+            compl_data.append(data)
+        return compl_data
 
 
     # try to eval code snippet in front of column and dynamically use dir() on eval result
@@ -26,25 +27,30 @@ async def get_completions(source, row, column, file):
     *_, expr = line.split(" ")
     prefix = ""
     if expr.endswith("."):
-      expr = expr[:-1]
+        expr = expr[:-1]
     elif "." in expr:
-      *front_parts, prefix = expr.split(".")
-      expr = ".".join(front_parts)
+        *front_parts, prefix = expr.split(".")
+        expr = ".".join(front_parts)
 
     result = await run_eval(expr)
     if result.isError: return []
 
     for key in dir(result.value):
-      val = getattr(result.value, key)
-      name = key
-      if (inspect.ismethod(val)):
-        type = "function"
-        name += str(inspect.signature(val))
-      else:
-        type = "instance"
-      compl_data.append({
-        "name": name, "type": type,
-        "module_name": expr, "prefix": prefix,
-        "priority": 1000
-      })
+        if len(prefix) > 0 and not key.startswith(prefix):
+            continue
+        try:
+            val = getattr(result.value, key)
+        except Exception:
+            continue;
+        name = key
+        if (inspect.ismethod(val)):
+            type = "function"
+            name += str(inspect.signature(val))
+        else:
+            type = "instance"
+        compl_data.append({
+            "name": name, "type": type,
+            "module_name": expr, "prefix": prefix,
+            "priority": 1000
+        })
     return compl_data
